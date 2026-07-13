0dc2a0538599e0e92eea08eb4985cacf4abfb474
Tanimara Elias Santos <tanimaraeliassantos@gmail.com>
2026-07-13 15:16:42 +0200

    refactor(tests): change in code to pass tests created

--- expense_calculator/expense_calculator/urls.py
+++ expense_calculator/expense_calculator/urls.py
@@ -25,6 +25,7 @@ router.register(r'expenses', views.ExpenseViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- expense_calculator/expenses/views.py
+++ expense_calculator/expenses/views.py
@@ -1,10 +1,38 @@
 
+import ast
+import json
+
+from django.http import QueryDict
+from rest_framework.parsers import BaseParser, FormParser, JSONParser, MultiPartParser
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import ExpenseSerializer
 from .models import Expense
 
+
+class OctetStreamParser(BaseParser):
+    media_type = 'application/octet-stream'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        body = stream.read().decode('utf-8')
+        if not body:
+            return {}
+
+        try:
+            return json.loads(body)
+        except json.JSONDecodeError:
+            try:
+                data = ast.literal_eval(body)
+            except (ValueError, SyntaxError):
+                return QueryDict(body, mutable=True)
+
+            if isinstance(data, dict):
+                return data
+            return QueryDict(body, mutable=True)
+
+
 class ExpenseViewSet(ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
-    
+    parser_classes = (JSONParser, FormParser,
+                      MultiPartParser, OctetStreamParser)
